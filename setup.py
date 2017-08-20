7e7eb48750d4404dcf1ccf3501d3fc829a7935a3
Victor Munoz <victorm@marshland.es>
2017-08-20 15:44:24 +0200

    Corrected small miskates.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     description="Feature flag management framework and utilities",
     author="Marshland",
     author_email="dev@marshland.es",
-    packages_dir={"": "src"},
+    package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
         "SQLAlchemy==1.1.13",
-        "zope.intereface==4.4.2",
+        "zope.interface==4.4.2",
     ],
     python_requires=">=3.5",
     zip_safe=False,
