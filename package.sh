1d8c2168fe90d7b758d3f7cff29906081e6731e3
Victor Munoz <victorm@marshland.es>
2017-09-17 22:34:33 +0200

    Created hidden dir fot pkgng-related files.

--- package.sh
+++ package.sh
@@ -7,4 +7,4 @@ rm -fr wheels
 pip wheel --wheel-dir=${WHEELS} .
 pip install --use-wheel --no-index --find-links=${WHEELS} ensign
 find ${PKG}usr/local/ensign -type f -or -type l | awk '{ sub(/pkgstage/, ""); print }' > plist
-pkg create -M .manifest -p plist -r ${PKG} -o .
+pkg create -M .pkgfiles/MANIFEST -p plist -r ${PKG} -o .
